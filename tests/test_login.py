04da2626768556320b16d4b3bb796d9ea795bb01
LuatNg <nvluathcmut@gmail.com>
2025-06-20 22:41:31 +0700

    Add fail case

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,3 +7,9 @@ def test_login_successful(setup):
     login_page = LoginPage(setup)
     login_page.login()
     assert "Logged In" in setup.title
+
+@pytest.mark.smoke
+def test_login_fail(setup):
+    login_page = LoginPage(setup)
+    login_page.login()
+    assert "Logged In" not in setup.title
